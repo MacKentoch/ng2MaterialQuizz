86038043fbbcb59a51b2db13c46d52ca087dbf44
MacKentoch <erwan.datin@gmail.com>
2016-03-16 06:50:57 +0100

    chore : first commit

---
+++ src/app/app.ts
@@ -0,0 +1,32 @@
+import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {FORM_PROVIDERS} from 'angular2/common';
+
+import '../style/app.scss';
+
+import {Api} from './services/api/api';
+import {Home} from './components/home/home';
+import {About} from "./components/about/about";
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app', // <app></app>
+  providers: [...FORM_PROVIDERS, Api],
+  directives: [...ROUTER_DIRECTIVES],
+  pipes: [],
+  styles: [require('./app.scss')],
+  template: require('./app.html')
+})
+@RouteConfig([
+  {path: '/', component: Home, name: 'Home'},
+  {path: '/About', component: About, name: 'About'}
+])
+export class App {
+  url: string = 'https://github.com/preboot/angular2-webpack';
+
+  constructor(public api: Api) {
+  }
+}
---
+++ src/bootstrap.ts
@@ -0,0 +1,33 @@
+import {enableProdMode, provide} from "angular2/core";
+import {bootstrap, ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/browser';
+import {ROUTER_PROVIDERS, HashLocationStrategy, LocationStrategy} from 'angular2/router';
+import {HTTP_PROVIDERS} from 'angular2/http';
+
+const ENV_PROVIDERS = [];
+// depending on the env mode, enable prod mode or add debugging modules
+if (process.env.ENV === 'prod') {
+  enableProdMode();
+} else {
+  ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
+}
+
+/*
+ * App Component
+ * our top level component that holds all of our components
+ */
+import {App} from './app/app';
+
+/*
+ * Bootstrap our Angular app with a top level component `App` and inject
+ * our Services and Providers into Angular's dependency injection
+ */
+document.addEventListener('DOMContentLoaded', function main() {
+  return bootstrap(App, [
+    // These are dependencies of our App
+    ...HTTP_PROVIDERS,
+    ...ROUTER_PROVIDERS,
+    ...ENV_PROVIDERS,
+    provide(LocationStrategy, {useClass: HashLocationStrategy}) // use #/ routes, remove this for HTML5 mode
+  ])
+  .catch(err => console.error(err));
+});
