9b499be1bc217fc4c6e502f4e2c213c96a3721ce
MacKentoch <erwan.datin@gmail.com>
2016-03-19 21:32:53 +0100

    chore home

--- src/app/views/home/home.ts
+++ src/app/views/home/home.ts
@@ -1,14 +1,23 @@
 import {Component, OnInit} from 'angular2/core';
 import {FORM_DIRECTIVES} from 'angular2/common';
+import {ViewsContainer} from '../../containers/views-container/views-container';
 
 @Component({
-  selector: 'home',
-  directives: [...FORM_DIRECTIVES],
-  pipes: [],
-  styles: [require('./home.scss')],
-  template: require('./home.html')
+  selector    : 'home',
+  template    : `
+  <views-container>
+    <h1>
+      {{title}}
+    </h1>
+  </views-container>
+  `,
+  styleUrls   : [require('./home.scss')],
+  providers   : [],
+  directives  : [ViewsContainer, ...FORM_DIRECTIVES],
+  pipes       : []
 })
 export class Home implements OnInit {
+  public title: string = 'Welcome to ng2MaterialQuizz';
 
   constructor() {
     // Do stuff
